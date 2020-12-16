94839a4ecd96a9ae3d5fe98d36c72f7cf1c72b4f
herickmotta <herickrossato@gmail.com>
2020-12-16 20:53:00 -0300

    search exams initial layout

--- src/pages/SearchExam.js
+++ src/pages/SearchExam.js
@@ -1,7 +1,85 @@
-import React from 'react'
+import React,{useState} from 'react'
+import styled from 'styled-components';
+import colors from '../components/colors';
 
-export default function SearchExam(){
-    return(
-        <h1>SearchExam</h1>
+export default function SearchExam() {
+    const [listBy,setListBy] = useState('subject');
+    const [exams,setExams] = useState([]);
+    return (
+        <StyledSearch>
+            <h1>Search a Exam</h1>
+            <ListByBox>
+                <div>List by:</div>
+                <select defaultValue="subject" onChange={(e) => setListBy(e.target.value)} name="listBy">
+                    <option value="subject">Subject</option>
+                    <option value="professor">Professor</option>
+                </select>
+            </ListByBox>
+
+            <ExamRepository>
+                {exams.length === 0 
+                    ?
+                    <EmptyBox>No exams found :(</EmptyBox>
+                    :
+                    ''
+                }
+            </ExamRepository>
+        </StyledSearch>
     );
 }
+
+const StyledSearch = styled.div`
+    width: 100vw;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    font-family:'Roboto',sans-serif;
+    font-weight:bold;
+    h1{
+        margin: 20px 0;
+        color: ${colors.lightblue};
+        font-size:28px;
+    }
+`
+
+const ExamRepository = styled.div`
+    display:flex;
+    flex-direction:column;
+    background: #FFF;
+    width:80%;
+    border-radius:10px;
+    margin: 15px 0;
+    min-height:200px;
+`
+const EmptyBox = styled.div`
+    flex-grow:1;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    font-weight: normal;
+    opacity:60%;
+`
+const ListByBox =  styled.div`
+    display:flex;
+    align-items:center;
+    color: #FFF;
+    background: ${colors.lightblue};
+    padding: 5px 10px;
+    border-radius:10px;
+    font-size: 18px;
+    font-weight: normal;
+    & >div{
+        margin-right:5px;
+    }
+    select{
+        background:inherit;
+        color:inherit;
+        border:none;
+        font-size:inherit;
+        font-weight:bold;
+        &:active,&:focus{
+            outline:none;
+            border:none;
+        }
+    }
+`
