b810a0b06b76ccc81de1ff0811aca9973b04fcce
herickmotta <herickrossato@gmail.com>
2020-12-16 18:59:42 -0300

    initial structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+export default function App(){
+    <Router>
+        <Switch>
+            <Route path='/' component={}></Route>
+            <Route path='/send' component={}></Route>
+            <Route path='/search' component={}></Route>
+        </Switch>
+    </Router>
+}
---
+++ src/components/colors.js
@@ -0,0 +1,7 @@
+const colors = {
+    lightblue: '#007AFF',
+    lightgray: '#DBDBDB',
+    black: '#000'
+}
+
+export default colors;
