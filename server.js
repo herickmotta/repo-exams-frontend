48dc24f9784c47610f62e9fe7ae46ffaeb35155f
herickmotta <herickrossato@gmail.com>
2020-12-18 18:59:53 -0300

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
