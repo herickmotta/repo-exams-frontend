b89b1e30670f2c359521ac1b0f776e8bab66f577
herickmotta <herickrossato@gmail.com>
2020-12-18 19:42:42 -0300

    add error message

--- src/pages/SendExam.js
+++ src/pages/SendExam.js
@@ -38,7 +38,7 @@ export default function SendExam(){
             alert('sucess');
             setButtonDisabled(false);
         }).catch(e=>{
-            alert(e.response.message);
+            alert(e.response.data.error);
             setButtonDisabled(false);
         });
 
