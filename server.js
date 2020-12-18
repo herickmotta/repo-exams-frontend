e6ba73204167cbfcc022d355b9a395762d0d7fb5
herickmotta <herickrossato@gmail.com>
2020-12-18 19:44:43 -0300

    remove comment

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.listen(process.env.PORT || 3000);
